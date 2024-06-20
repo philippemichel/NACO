0d84f9dd4374ea2a35cfcc9ed54db586bda3da31
Philippe MICHEL <philippe@docteur-michel.fr>
2024-06-20 11:59:45 +0200

    Critère principal OK

--- README.md
+++ README.md
@@ -12,4 +12,12 @@
 
 Les données qui seront présentes ici seront fausses & présentes uniquement pour pouvoir tester les scripts.       
 
+# Plan d'analyse
 
+# Rapport statistique
+
+**import.R** : procédure d'import & de mise en forme des données. La mise en palce des labels des variables ne sera implémentée qu'en fin d'analyse.
+
+**stat_naco_1.qmd** Format pdf (vide, ne sera utilisé qu'en fiin d'analyse)
+
+**stat_naco_1h.qmd** Format html 
--- import.R
+++ import.R
@@ -17,9 +17,13 @@ library(janitor)
 library(lubridate)
 library(baseph)
 #
+limg <- dmy("30/09/2021")
 nna <- c("NA",""," ","AUCUN", "Abs","na","Na")
 tt <- read_ods("datas/stopnaco.ods", na = nna) |>
     clean_names() |>
+  mutate(date_entree = mdy_hm(date_entree)) |> 
+  mutate(date_incision = mdy_hm(date_incision)) |>
+  mutate(delai_chirurgical = as.numeric(difftime(date_incision, date_entree, units = "hours"))) |>
     mutate_if(is.character, as.factor) |> 
 mutate(type_fracture = fct_recode(type_fracture,
            "Col" = "Col 1",
@@ -35,7 +39,20 @@ mutate(transfert = ifelse((transfert_beaumont == "yes") | (transfert_magny == "y
                                  dig.lab = 4,
                                  breaks = c(0, 0.1, 2), 
                                  labels = c("no", "yes"))) |> 
-    mutate(score_asa = as.factor(score_asa))
+    mutate(score_asa = as.factor(score_asa)) |> 
+   mutate(charlson = cut(charlson,
+                           include.lowest = TRUE,
+                           right = FALSE,
+                           dig.lab = 4,
+                           breaks = c(-1, 4, 6, 9, 15),
+                           labels = c("<4", "4-5", "6-9", "10 et +"))) |> 
+  mutate(groupe = ifelse(date_entree < limg, "avant", "après")) |> 
+  mutate(groupe = as.factor(groupe)) |>
+  mutate(groupe =   fct_relevel(groupe,
+    "avant", "après"
+  )) |> 
+  mutate(delai48 =as.factor(ifelse(delai_chirurgical>48,"> 48 h", "< 48 h")))
+
   
   
   
